060d74655bbf5eebe00487de9e0ce832479d5044
2bit <2bitpng@gmail.com>
2023-05-12 09:03:15 +0900

    change c c++

---
+++ mycc.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main(int argc, char *argv[]){
+  if(argc!=2){
+    std::cerr<<"引数の個数が正しくありません"<<std::endl;
+    return 1;
+  }
+  std::cout<<".intel_syntax noprefix"<<std::endl;
+  std::cout<<".globl main"<<std::endl;
+  std::cout<<"main:"<<std::endl;
+  std::cout<<"  mov rax, "<<atoi(argv[1])<<std::endl;
+  std::cout<<"  ret"<<std::endl;
+  return 0;
+}
